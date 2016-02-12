f1b420fd8f54806d3e0c2e5487ca71c56ad4ca55
Vyacheslav Horbach <vyacheslav@Vyacheslavs-MacBook-Air.local>
2016-02-12 17:33:56 +0000

    finished the functions' excercise

---
+++ FunctionsExcercise.playground/Contents.swift
@@ -0,0 +1,40 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+// Adding
+
+func add(one: Double, two: Double) {
+    one + two
+}
+
+add(1, two: 3.13)
+
+
+// Subtraction
+
+func subtract(numberOne: Int, numberTwo: Int) {
+    numberOne - numberTwo
+}
+
+subtract(4, numberTwo: 1)
+
+
+// Multiplication
+
+func multiply(numberOne: Float, numberTwo: Float) {
+    numberOne * numberTwo
+}
+
+multiply(3, numberTwo: 4.1)
+
+
+// Division
+
+func divide(numberOne: Double, numberTwo: Double) {
+    numberOne / numberTwo
+}
+
+divide(15, numberTwo: 2)
+
