fc439c5e2f3413ef13af9af483cbec5aa38f8562
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-02-20 00:07:19 +0000

    practised functions

---
+++ WarmUpSection3.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func isEven(number: Int) -> Bool {
+    if number % 2 == 0 {
+        return true
+    } else {
+        return false
+    }
+}
+
+isEven(4)
+
+isEven(7)
