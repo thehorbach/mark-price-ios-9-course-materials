006f31452b9b726256f5791f51e99781769a80d5
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-02-27 12:16:17 +0000

    finished warup for lecture 4

---
+++ Warmup - BooYa.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+for x in 1...200 {
+    if x % 3 == 0 && x % 5 == 0 {
+        print("BooYa")
+    } else if x % 3 == 0 {
+        print("Boo")
+    } else if x % 5 == 0 {
+        print("Ya")
+    }
+}
+
